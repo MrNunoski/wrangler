15d3020a719f5a7b1ef18cf188de42ea140f07ce
Korey Kassir <kkassir@popsugar.com>
2015-03-16 14:31:28 -0700

    redirect for old site

--- server/publications.js
+++ server/publications.js
@@ -1,36 +1,36 @@
-Meteor.publish('tickets', function() {
-  var currentMilestone = Milestones.findOne({current: true});
-  if (currentMilestone) {
-    fixVersionName = currentMilestone.name;
-  }
-  else {
-    return Tickets.findOne();
-  }
+// Meteor.publish('tickets', function() {
+//   var currentMilestone = Milestones.findOne({current: true});
+//   if (currentMilestone) {
+//     fixVersionName = currentMilestone.name;
+//   }
+//   else {
+//     return Tickets.findOne();
+//   }
 
-  return Tickets.find({$or: [
-  { "fixVersion.name": fixVersionName, statusName: {$in: ["Done", "Verified on Dev"]} },
-    {isRegression: true}
-  ]});
+//   return Tickets.find({$or: [
+//   { "fixVersion.name": fixVersionName, statusName: {$in: ["Done", "Verified on Dev"]} },
+//     {isRegression: true}
+//   ]});
 
-  // return Tickets.find({jiraId: {$nin: [null, undefined]}});
-});
+//   // return Tickets.find({jiraId: {$nin: [null, undefined]}});
+// });
 
-Meteor.publish('testscripts', function(ticketJiraId) {
-  return Testscripts.find({ ticketJiraId: ticketJiraId }, {sort: {testscriptNum: 1}});
-});
+// Meteor.publish('testscripts', function(ticketJiraId) {
+//   return Testscripts.find({ ticketJiraId: ticketJiraId }, {sort: {testscriptNum: 1}});
+// });
 
-Meteor.publish('milestones', function() {
-  return Milestones.find();
-});
+// Meteor.publish('milestones', function() {
+//   return Milestones.find();
+// });
 
-Meteor.publish('stream', function() {
-  return Stream.find();
-});
+// Meteor.publish('stream', function() {
+//   return Stream.find();
+// });
 
-Meteor.publish('userData', function() {
-  return Meteor.users.find({}, {fields: {username: 1, isAdmin: 1}});
-});
+// Meteor.publish('userData', function() {
+//   return Meteor.users.find({}, {fields: {username: 1, isAdmin: 1}});
+// });
 
-Meteor.publish('testingAssignments', function() {
-  return TestingAssignments.find();
-});
+// Meteor.publish('testingAssignments', function() {
+//   return TestingAssignments.find();
+// });
